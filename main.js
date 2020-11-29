50884e7678e778ea2661577cdb8f33a4d6fc4b4a
Heiko Dotzauer <heiko@dtzr.de>
2020-11-29 20:19:07 +0100

    my_link_list added

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
+const my_link_list = [{
+    "link": "http://www.spiegel.de",
+    "text": "Drück mich, wenn Du Dich traust :-)"
+},
+{
+    "link": "http://www.spiegel.de",
+    "text": "Bin der zweite Link - für Tag 2"
+},
+]
+
 // const calendarButton = document.querySelector(".btn-start");
 const calendarContainer = document.querySelector(".container");
 
 const calendarDays = 24;
 
 const openDoor = (path, event) => {
+    var pos_of_minus = `${path}`.search('-') + 1
+    var day = `${path}`.substring(pos_of_minus)
+    console.log('day: ' + day)
     event.target.parentNode.style.backgroundImage = `url(${path}.jpg)`;
     event.target.style.opacity = "0";
     event.target.style.backgroundColor = "#521751";
+
+    // Setze URL Link
+    // var url_link = JSON.parse(my_link_list);
+    my_link = my_link_list[day - 1]
+    console.log('my_link: ' + my_link)
+    if (my_link != null) {
+        event.target.parentNode.innerHTML = my_link_list[day - 1].text.link(my_link_list[day - 1].link);
+    }
+    // spiele Audio (falls vorhanden)
     var audio = new Audio(`${path}.mp3`);
     audio.play();
 }
